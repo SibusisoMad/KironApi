9bf3e918e906305c5fb6bc98e2eeea84ec693cb4
Sibusiso Madloba <smadloba@gmail.com>
2023-07-20 19:40:32 +0200

    DB connect manager class

---
+++ DataLayer/DAL/DBConnectionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DAL
+{
+    public class DBConnectionManager
+    {
+        private readonly string connectionString;
+
+        public DBConnectionManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
